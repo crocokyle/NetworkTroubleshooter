f4fc57e0fbee7f2d2a434777640d02a82c79ae5a
crocokyle <me@kyle-dixon.com>
2022-10-21 22:39:00 -0400

    fix: :goal_net: Catch address format errors to raise explicit exception

--- discovery.py
+++ discovery.py
@@ -2,7 +2,7 @@ from endpoint import Endpoint
 from utils import run_command, get_client_net_details
 
 import re
-from netaddr import IPAddress
+from netaddr import IPAddress, AddrFormatError
 
 def _parse_tracert(command_output:str) -> list[IPAddress]:
     route_pattern = r"over a maximum of 30 hops:\s+(.*)\nTrace complete."
@@ -14,7 +14,10 @@ def _parse_tracert(command_output:str) -> list[IPAddress]:
         if line:
             dirty_ip = line.split()[-1]
             clean_ip = dirty_ip.replace('[', '').replace(']', '')
-            hops.append(IPAddress(clean_ip))
+            try:
+                hops.append(IPAddress(clean_ip))
+            except AddrFormatError:
+                raise Exception(f'Invalid IP address {clean_ip}')
     
     return hops
 
