d27d9d9f2b709d46e86bc96612d7c57de4877f55
crocokyle <me@kyle-dixon.com>
2022-10-21 20:57:29 -0400

    feat: discovery boilerplate code

---
+++ discovery.py
@@ -0,0 +1,9 @@
+from endpoint import Endpoint
+from utils import run_command
+
+def get_internal_endpoints(localhost_ip: str) -> list[Endpoint]:
+    pass
+    
+def get_localhost_ip() -> str:
+    pass
+
