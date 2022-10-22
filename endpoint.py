9bb9af939e0ba91b4af43f25ba9f2619805af0e7
crocokyle <me@kyle-dixon.com>
2022-10-21 21:17:09 -0400

    refactor: Better SRP by moving main methods to main module

--- endpoint.py
+++ endpoint.py
@@ -1,7 +1,4 @@
 import re
-from tqdm import tqdm
-from pprint import pprint
-
 from utils import run_command
 
 class Endpoint:
@@ -25,33 +22,3 @@ class Endpoint:
             self.status = "Error parsing ping results!"
 
         return self.status, self.packet_loss
-
-
-def validate_config(endpoint_config:list[dict]) -> list[Endpoint]:
-    return [Endpoint(k, v) for d in endpoint_config for k, v in d.items()]
-
-def test_endpoints(endpoints: list[Endpoint]) -> list[Endpoint]:
-    for endpoint in tqdm(endpoints):
-        endpoint.test()
-
-    return endpoints
-
-
-if __name__ == "__main__":
-    unit_test = b'\r\nPinging 8.8.8.8 with 32 bytes of data:\r\nReply from 8.8.8.8: bytes=32 time=79ms TTL=110\r\nReply from 8.8.8.8: bytes=32 time=42ms TTL=110\r\nReply from 8.8.8.8: bytes=32 time=60ms TTL=110\r\nReply from 8.8.8.8: bytes=32 time=50ms TTL=110\r\n\r\nPing statistics for 8.8.8.8:\r\n    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),\r\nApproximate round trip times in milli-seconds:\r\n    Minimum = 42ms, Maximum = 79ms, Average = 57ms\r\n'.decode('utf-8')
-
-    endpoint_config = [
-        {"internet": '8.8.8.8'},
-        {"dns": 'google.com'},
-        {"modem": '192.168.12.1'},
-        {"router": '192.168.86.1'},
-    ]
-
-    validated_endpoints = validate_config(endpoint_config)
-    tested_endpoints = test_endpoints(validated_endpoints)
-    for endpoint in tested_endpoints:
-        print(f"\n================== {endpoint.name} ==================")
-        print(f"IP: {endpoint.ip}")
-        print(f"Packet Loss: {endpoint.packet_loss}%")
-        print(f"Status: {endpoint.status}")
-        print("="*(38+len(endpoint.name)))
