266e4fdfc7892085547fdb21f8ba997271b17b07
crocokyle <me@kyle-dixon.com>
2022-10-22 23:55:49 -0400

    refactor: :boom: Move discovery module to use datacollector - broken - WIP

--- data_collectors/network/discovery.py
+++ data_collectors/network/discovery.py
@@ -1,7 +1,7 @@
 # Local imports
 from utils.operating_system import legacy_run_command
 from utils.network import Endpoint, is_same_24_subnet
-from data_collectors.network.local import get_client_net_info
+from data_collectors.abstract import DataCollector
 
 # 3rd party imports
 import re
@@ -9,40 +9,48 @@ from netaddr import IPAddress, AddrFormatError
 from PyQt5.QtCore import QProcess
 
 
-def _parse_tracert(command_output:str) -> list[IPAddress]:
-    route_pattern = r"over a maximum of 30 hops:\s+(.*)\nTrace complete."
-    output = re.search(route_pattern, command_output, flags=re.MULTILINE|re.DOTALL).group(1)
-    lines = output.splitlines()
+# TODO: Specify Windows/linux - maybe use remote rather than discovery
+class DicoveryDataCollector(DataCollector):
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def _parse_tracert(command_output:str) -> list[IPAddress]:
+        route_pattern = r"over a maximum of 30 hops:\s+(.*)\nTrace complete."
+        output = re.search(route_pattern, command_output, flags=re.MULTILINE|re.DOTALL).group(1)
+        lines = output.splitlines()
+        
+        hops = []
+        for line in lines:
+            if line:
+                ip_field = line.split()[-1].replace('[', '').replace(']', '')
+                if re.match(r'(?:[0-9]{1,3}\.){3}[0-9]{1,3}', ip_field):
+                    try:
+                        hops.append(IPAddress(ip_field))
+                    except AddrFormatError:
+                        raise Exception(f'Invalid IP address {ip_field}')
+        
+        return hops
+
+    # TODO: cleanup unused params
+    def get_private_ips(self, console_process: QProcess) -> list[IPAddress]:
+        tracert_output = legacy_run_command(['tracert', '8.8.8.8'])
+        hops = self._parse_tracert(tracert_output)
+        internal_ips = []
+        for hop in hops:
+            if hop.is_private():
+                internal_ips.append(hop)
+
+        return internal_ips
+
+    # TODO: cleanup unused params
+    def get_internal_endpoints(self, console_process: QProcess) -> list[Endpoint]:
+        private_ips: list[IPAddress] = self.get_private_ips(console_process)
+        # TODO: DO SOMETHING client_info:dict[str:str] = get_client_net_info()
     
-    hops = []
-    for line in lines:
-        if line:
-            ip_field = line.split()[-1].replace('[', '').replace(']', '')
-            if re.match(r'(?:[0-9]{1,3}\.){3}[0-9]{1,3}', ip_field):
-                try:
-                    hops.append(IPAddress(ip_field))
-                except AddrFormatError:
-                    raise Exception(f'Invalid IP address {ip_field}')
-    
-    return hops
-
-def get_private_ips(console_process: QProcess) -> list[IPAddress]:
-    tracert_output = legacy_run_command(['tracert', '8.8.8.8'])
-    hops = _parse_tracert(tracert_output)
-    internal_ips = []
-    for hop in hops:
-        if hop.is_private():
-            internal_ips.append(hop)
-
-    return internal_ips
-
-def get_internal_endpoints(console_process: QProcess) -> list[Endpoint]:
-    private_ips: list[IPAddress] = get_private_ips(console_process)
-    client_info:dict[str:str] = get_client_net_info()
-
-    for ip in private_ips:
-        if is_same_24_subnet(ip, client_info.get('ip')):
-            router_endpoint = Endpoint('router', ip)
+    def run(self):
+        # TODO: implement
+        super().run()
     
 def get_localhost_ip() -> str:
     pass
