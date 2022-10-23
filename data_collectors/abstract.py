ebf6fb780faea9e6ce9ad0861c3e34bea97cbae4
crocokyle <me@kyle-dixon.com>
2022-10-22 23:26:04 -0400

    refactor: :recycle: DataCollectors to use new data structure

--- data_collectors/abstract.py
+++ data_collectors/abstract.py
@@ -1,16 +1,16 @@
 # Local imports
 from utils.credentials import Credential
+from utils.data_structures import Data
 
 # 3rd party imports
 from abc import ABC, abstractmethod
 from datetime import datetime
-from typing import Any
 
 
 class DataCollector(ABC):
     def __init__(self):
         self.last_run = datetime.fromtimestamp(0)
-        self.data: Any = None
+        self.data: Data = Data()
         self.target: str = ""
         self.credentials: Credential = None
 
--- data_collectors/network/local.py
+++ data_collectors/network/local.py
@@ -1,6 +1,6 @@
 # Local imports
 from utils.operating_system import legacy_run_command
-from data_collectors.interface import DataCollector
+from data_collectors.abstract import DataCollector
 
 # 3rd party imports
 import psutil
@@ -21,24 +21,24 @@ class WindowsDataCollector(DataCollector):
 
     @staticmethod
     def _parse_ipconfig_property(property: str, cmd_output: str) -> str:
-            property_value = None
-            property_pattern = re.escape(property) + "[ \.]+: ((?:[\d]{1,3}.){3}[\d]{1,3})"
-            if match := re.search(property_pattern, cmd_output):
-                property_value = match.group(1)
+        property_value = None
+        property_pattern = re.escape(property) + "[ \.]+: ((?:[\d]{1,3}.){3}[\d]{1,3})"
+        if match := re.search(property_pattern, cmd_output):
+            property_value = match.group(1)
 
-            return property_value
+        return property_value
 
     def get_client_net_info(self) -> None:
         hostname = socket.gethostname()
         ip_address = socket.gethostbyname(hostname)
         ip_config_output = legacy_run_command(['ipconfig', '/all'])
 
-        self.data.network.hostname = hostname
-        self.data.network.connected_ip = ip_address
-        self.data.network.connected_interface = self._get_connected_interface(ip_address)
-        self.data.network.dns_server = self._parse_ipconfig_property("DNS Servers", ip_config_output)
-        self.data.network.gateway = self._parse_ipconfig_property("Default Gateway", ip_config_output)
-        self.data.network.dhcp_server = self._parse_ipconfig_property("DHCP Server", ip_config_output)
+        self.data.Network.HOSTNAME.val = hostname
+        self.data.Network.CONNECTED_IP.val = ip_address
+        self.data.Network.CONNECTED_INTERFACE.val = self._get_connected_interface(ip_address)
+        self.data.Network.DNS_SERVER.val = self._parse_ipconfig_property("DNS Servers", ip_config_output)
+        self.data.Network.GATEWAY.val = self._parse_ipconfig_property("Default Gateway", ip_config_output)
+        self.data.Network.DHCP_SERVER.val = self._parse_ipconfig_property("DHCP Server", ip_config_output)
 
     def run(self):
         self.get_client_net_info()
@@ -47,4 +47,9 @@ class WindowsDataCollector(DataCollector):
 if __name__ == '__main__':
     local_dc = WindowsDataCollector()
     local_dc.run()
-    print(local_dc.data)
+    
+    for fact in local_dc.data.Network:
+        print("===========================")
+        print(f'Fact description: {fact.description}')
+        print(f'Value: {fact.val}')
+        print("===========================\n")
